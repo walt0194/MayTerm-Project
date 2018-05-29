f93649585b4949b50d9a4ef68c0c68d2468af57b
walt0194 <waltb18082@isd273.org>
2018-05-29 12:16:52 -0500

    Set up projects for java and swift

---
+++ MayTermSwift/MayTermProject/MayTermProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MayTermProject
+//
+//  Created by Walt Boettge on 5/29/18.
+//  Copyright © 2018 Walt Boettge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
