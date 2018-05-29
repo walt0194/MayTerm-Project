8ed6c00133ece7b267d629d0402842a43f2c134a
walt0194 <waltb18082@isd273.org>
2018-05-29 13:11:37 -0500

    Made a basic app, and installed it

--- MayTermSwift/MayTermProject/MayTermProject/ViewController.swift
+++ MayTermSwift/MayTermProject/MayTermProject/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var onLabel: UILabel!
+    @IBOutlet weak var offLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if(onLabel.isHidden){
+            onLabel.isHidden = false
+            offLabel.isHidden = true
+        }
+        else{
+            onLabel.isHidden = true
+            offLabel.isHidden = false
+        }
     }
-
+    
 
 }
 
